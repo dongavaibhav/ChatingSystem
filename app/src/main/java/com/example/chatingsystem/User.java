0f2b0fd3c63793b8dbe98e6ded9bd2aebde763f9
dongavaibhav <dongavaibhav11@gmail.com>
2020-03-04 16:54:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatingsystem/User.java
@@ -0,0 +1,110 @@
+package com.example.chatingsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class User extends AppCompatActivity {
+
+    // Declare the variable
+    ListView usersList;
+    TextView noUsersText;
+    ArrayList<String> al = new ArrayList<>();
+    int totalUsers = 0;
+    ProgressDialog pd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+       findId();
+
+        pd = new ProgressDialog(User.this);
+        pd.setMessage("Loading...");
+        pd.show();
+
+        String url = "https://chatingsystem-1ee10.firebaseio.com/users.json";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String s) {
+                doOnSuccess(s);
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                System.out.println("" + volleyError);
+            }
+        });
+
+        RequestQueue rQueue = Volley.newRequestQueue(User.this);
+        rQueue.add(request);
+
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                UserDetails.chatWith = al.get(position);
+                startActivity(new Intent(User.this, Chat.class));
+            }
+        });
+    }
+
+    // find layout id
+    public void findId() {
+        usersList = (ListView)findViewById(R.id.usersList);
+        noUsersText = (TextView)findViewById(R.id.noUsersText);
+    }
+
+    // Listview Visibility Hide gone
+    public void doOnSuccess(String s){
+        try {
+            JSONObject obj = new JSONObject(s);
+
+            Iterator i = obj.keys();
+            String key = "";
+
+            while (i.hasNext()) {
+                key = i.next().toString();
+
+                if (!key.equals(UserDetails.username)) {
+                    al.add(key);
+                }
+                totalUsers++;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (totalUsers <=1) {
+            noUsersText.setVisibility(View.VISIBLE);
+            usersList.setVisibility(View.GONE);
+        }
+        else {
+            noUsersText.setVisibility(View.GONE);
+            usersList.setVisibility(View.VISIBLE);
+            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+        }
+        pd.dismiss();
+    }
+}
---
+++ app/src/main/java/com/example/chatingsystem/UserDetails.java
@@ -0,0 +1,10 @@
+package com.example.chatingsystem;
+
+// Details of User
+public class UserDetails {
+    public static int Device_Width;
+
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
